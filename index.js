dad12f193538b836ec076c0ffdb918979b8a5736
rafi00000 <delwar2021bd@gmail.com>
2023-10-18 13:46:29 +0600

    finished post product to mongodb opation

--- index.js
+++ index.js
@@ -7,6 +7,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://delwar2021bd:abjqmFGOZFbsbGoR@cluster0.mn7153h.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db("shopDB").collection('products');
+
+    app.get('/product', async(req, res) =>{
+        const cursor = await productCollection.find().toArray();
+        res.send(cursor)
+    })
+
+    app.post('/product', async(req, res)=>{
+        const item = req.body;
+        console.log('product insert req: ', item);
+        const result = await productCollection.insertOne(item);
+        res.send(result);
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=>{
     res.send('The server is running');
